2a65eb376672eb264ab7c13845e71b7217f70bc2
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-09-14 15:27:01 +0900

    [#1] 9. Dynamic Stack View

--- Personal-Learning/YagomDotNet-Conquer-AutoLayout/DynamicStackView/DynamicStackView/StackViewController.swift
+++ Personal-Learning/YagomDotNet-Conquer-AutoLayout/DynamicStackView/DynamicStackView/StackViewController.swift
@@ -8,13 +8,16 @@
 import UIKit
 
 class StackViewController: UIViewController {
+    
+    var vertical: UIStackView = UIStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let vertical = UIStackView()
         vertical.axis = .vertical
         vertical.translatesAutoresizingMaskIntoConstraints = false
+        vertical.spacing = 10
+        vertical.distribution = .fillEqually
         view.addSubview(vertical)
         
         let horizontal = UIStackView()
@@ -25,10 +28,12 @@ class StackViewController: UIViewController {
         let addbutton = UIButton()
         addbutton.setTitle("Add", for: .normal)
         addbutton.setTitleColor(.blue, for: .normal)
+        addbutton.addTarget(self, action: #selector(addView), for: .touchUpInside)
         
         let removeButton = UIButton()
         removeButton.setTitle("Remove", for: .normal)
         removeButton.setTitleColor(.blue, for: .normal)
+        removeButton.addTarget(self, action: #selector(removeView), for: .touchUpInside)
         
         horizontal.addArrangedSubview(addbutton)
         horizontal.addArrangedSubview(removeButton)
@@ -45,4 +50,27 @@ class StackViewController: UIViewController {
     }
     
 
+    @objc func addView() {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.isHidden = true
+        vertical.addArrangedSubview(view)
+        
+        UIView.animate(withDuration: 0.3) {
+            view.isHidden = false
+        }
+    }
+    
+    @objc func removeView() {
+        guard let last = vertical.arrangedSubviews.last else {
+            return
+        }
+        UIView.animate(withDuration: 0.3) {
+            last.isHidden = true
+        } completion: { (_) in
+            self.vertical.removeArrangedSubview(last)
+        }
+        
+        
+    }
 }
